cd41f0f398712e43cb454c8b9350e355702ab3bc
kinguser-star <jj.pp33@hotmail.com>
2025-03-04 22:24:57 -0500

    after adding everything in networking and creating vm with main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "my_vm" {
 ami = "ami-05b10e08d247fb927"
 subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
-instance_type = "t3.micro"
+key_name = data.terraform_remote_state.network_details.outputs.aws_key_pair
+vpc_security_group_ids = [data.terraform_remote_state.network_details.outputs.my_sg]
+instance_type = "t2.micro"
 tags = {
 Name = "pankajvm"
 }
